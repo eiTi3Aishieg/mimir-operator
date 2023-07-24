94080e3658ecc87ac149b27a2247e80a943d19a0
AmX <contact@amx.sh>
2023-07-24 09:10:24 +0200

    feat(mimirrules): status + reload on prometheusrules change

--- controllers/mimirrules_controller.go
+++ controllers/mimirrules_controller.go
@@ -3,13 +3,17 @@ package controllers
 import (
 	"context"
 	"fmt"
+	prometheus "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"mimir-operator/internal/utils"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	domain "mimir-operator/api/v1alpha1"
 )
@@ -58,7 +62,7 @@ func (r *MimirRulesReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		if controllerutil.ContainsFinalizer(mr, mimirFinalizer) {
 			if err := r.handleDeletion(ctx, mr); err != nil {
 				// Status is set only on failure to delete (the status is going to be deleted anyway if it succeeds)
-				return ctrl.Result{}, r.SetStatus(ctx, mr, err)
+				return ctrl.Result{}, r.setStatus(ctx, mr, err)
 			}
 
 			// Remove our finalizer from the list and update it
@@ -67,16 +71,16 @@ func (r *MimirRulesReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		}
 	}
 
-	return ctrl.Result{}, r.handleReconcile(ctx, mr)
+	return ctrl.Result{}, r.handleCreationAndChanges(ctx, mr)
 }
 
-// handleReconcile handles reconciliation of MimirRules for events that are not a deletion
+// handleCreationAndChanges handles reconciliation of MimirRules for events that are not a deletion
 // This means that this function will be called for any modification in a MimirRules or for
 // any creation of a new MimirRules in the API. It is also called periodically for scheduled
 // reconciliation and at the startup of the controller.
-func (r *MimirRulesReconciler) handleReconcile(ctx context.Context, mr *domain.MimirRules) error {
+func (r *MimirRulesReconciler) handleCreationAndChanges(ctx context.Context, mr *domain.MimirRules) error {
 	reconciliationError := r.reconcileRules(ctx, mr)
-	if err := r.SetStatus(ctx, mr, reconciliationError); err != nil {
+	if err := r.setStatus(ctx, mr, reconciliationError); err != nil {
 		return err
 	}
 
@@ -107,10 +111,34 @@ func (r *MimirRulesReconciler) reconcileRules(ctx context.Context, mr *domain.Mi
 	return r.syncRulesToRuler(ctx, auth, mr)
 }
 
-// SetStatus updates the status of MimirRules after reconciliation
+// reconcileOnPrometheusRuleChange sends a reconcile request to EVERY MimirRule on the cluster
+// This is done to retrigger the synchronization of MimirRules if new PrometheusRules have been added
+// or if some PrometheusRules have changed their definition
+func (r *MimirRulesReconciler) reconcileOnPrometheusRuleChange(rule client.Object) []reconcile.Request {
+	allMimirRules := &domain.MimirRulesList{}
+	err := r.List(context.Background(), allMimirRules)
+	if err != nil {
+		log.FromContext(context.TODO()).Error(err, "failed to list all MimirRules after a PrometheusRule change")
+		return []reconcile.Request{}
+	}
+
+	requests := make([]reconcile.Request, len(allMimirRules.Items))
+	for i, item := range allMimirRules.Items {
+		requests[i] = reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      item.GetName(),
+				Namespace: item.GetNamespace(),
+			},
+		}
+	}
+
+	return requests
+}
+
+// setStatus updates the status of MimirRules after reconciliation
 // If err is not nil, the error field is populated with the error and the status is set as "Failed"
 // Otherwise, status is set as "Synced"
-func (r *MimirRulesReconciler) SetStatus(ctx context.Context, mr *domain.MimirRules, err error) error {
+func (r *MimirRulesReconciler) setStatus(ctx context.Context, mr *domain.MimirRules, err error) error {
 	if err != nil {
 		mr.Status.Status = "Failed"
 		mr.Status.Error = err.Error()
@@ -126,5 +154,8 @@ func (r *MimirRulesReconciler) SetStatus(ctx context.Context, mr *domain.MimirRu
 func (r *MimirRulesReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&domain.MimirRules{}).
+		Watches( // Setup WATCH on PrometheusRules to dynamically reload MimirRules into the MimirRuler if a selected rule has been changed
+			&source.Kind{Type: &prometheus.PrometheusRule{}},
+			handler.EnqueueRequestsFromMapFunc(r.reconcileOnPrometheusRuleChange)).
 		Complete(r)
 }
