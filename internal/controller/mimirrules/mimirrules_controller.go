eb8ca7e04722ba8c4164fd6d1407665600e3b4ed
gign0766 <brewal44@gmail.com>
2024-04-18 08:51:13 +0200

    feat(mimirrules): add concurrency on reconciliation

--- internal/controller/mimirrules/mimirrules_controller.go
+++ internal/controller/mimirrules/mimirrules_controller.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -180,5 +181,8 @@ func (r *MimirRulesReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Watches( // Setup WATCH on PrometheusRules to dynamically reload MimirRules into the MimirRuler if a selected rule has been changed
 			&prometheus.PrometheusRule{},
 			handler.EnqueueRequestsFromMapFunc(r.reconcileOnPrometheusRuleChange)).
+		WithOptions(controller.Options{
+			MaxConcurrentReconciles: 32,
+		}).
 		Complete(r)
 }
